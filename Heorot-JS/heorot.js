9b0f97db7b3726be773dac1768b84478ad02a116
Eric Stensland <estensland88@gmail.com>
2014-09-09 12:29:02 -0500

    Update heorot.js

--- Heorot-JS/heorot.js
+++ Heorot-JS/heorot.js
@@ -1,76 +1,130 @@
-Heorot = {
-  
-  each: function(array, func){
-    for (var i=0, ii = array.length; i < ii; i ++) {
-      func(array[i])
+window.heorot = (function(){
+  var Heorot = function (array) {
+    for(var i = 0; i < array.length; i++ ) {
+      this[i] = array[i];
     }
-  },
+    this.length = array.length;
+  }
 
-  map: function(array, func){
-    returnArray = []
-    for (var i=0, ii = array.length; i < ii; i ++) {
-      console.log(i)
-      returnArray.push(func(array[i]))
-    }
-    return returnArray
-  },
-  
-  sortBySortAttr: function (parent, sortedSelector, keySelector) {
-    var sections = $(parent).find(sortedSelector).sort(function(a, b) {
-      var vA = Paths.grabTextParseInt($(keySelector, a));
-      var vB = Paths.grabTextParseInt($(keySelector, b));
-      return (vA < vB) ? -1 : (vA > vB) ? 1 : 0;
-    });
-    $(parent).append(sections);
-  },
-  
-  scrollHelper : {
-    scrollCheck: function(stationaryElement, event, amount){
-      if (this.insideXAxisCheck(stationaryElement, event)) {
-        var topOfElement = stationaryElement.offset().top
+  var heorot = {
+    t: function(array){
+      return new Heorot(array);
+    },
 
-        if (topOfElement > event.pageY){
-          this.scrollListUp(stationaryElement, amount);
-        }
-
-        else if (topOfElement + stationaryElement.height() < event.pageY) {
-          this.scrollListDown(stationaryElement, amount);
-        }
+    each: function(array, func){
+      for (var i=0, ii = array.length; i < ii; i ++) {
+        func(array[i]);
       }
     },
 
-    insideXAxisCheck: function(stationaryElement, event) {
-      var offset = stationaryElement.offset()
-      var leftOfElement = offset.left
-      var rightOfElement = offset.left + stationaryElement.width()
-
-      return (event.pageX < rightOfElement && event.pageX > leftOfElement)
+    map: function(array, func){
+      returnArray = []
+      for (var i=0, ii = array.length; i < ii; i ++) {
+        returnArray.push(func(array[i]));
+      }
+      return returnArray
     },
 
-    scrollListUp: function(stationaryElement, amount) {
-      stationaryElement.scrollTop(stationaryElement.scrollTop() - amount)
+    select: function(array, func){
+      returnArray = []
+      for (var i=0, ii = array.length; i < ii; i ++) {
+        if (func(array[i])){
+          returnArray.push(array[i]);
+        }
+      }
+      return returnArray
     },
 
-    scrollListDown: function(stationaryElement, amount) {
-      stationaryElement.scrollTop(stationaryElement.scrollTop() + amount)
-    }
-  },
-  
-  browserSubmit: function (action, method, input) {
-    var form = $('<form />', {
+    reject: function(array, func){
+      returnArray = []
+      for (var i=0, ii = array.length; i < ii; i ++) {
+        if (!func(array[i])){
+          returnArray.push(array[i]);
+        }
+      }
+      return returnArray;
+    },
+    
+    sortBySortAttr: function (parent, sortedSelector, keySelector) {
+      var sections = $(parent).find(sortedSelector).sort(function(a, b) {
+        var vA = Paths.grabTextParseInt($(keySelector, a));
+        var vB = Paths.grabTextParseInt($(keySelector, b));
+        return (vA < vB) ? -1 : (vA > vB) ? 1 : 0;
+      });
+      $(parent).append(sections);
+    },
+    
+    scrollHelper : {
+      scrollCheck: function(stationaryElement, event, amount){
+        if (this.insideXAxisCheck(stationaryElement, event)) {
+          var topOfElement = stationaryElement.offset().top
+
+          if (topOfElement > event.pageY){
+            this.scrollListUp(stationaryElement, amount);
+          }
+
+          else if (topOfElement + stationaryElement.height() < event.pageY) {
+            this.scrollListDown(stationaryElement, amount);
+          }
+        }
+      },
+
+      insideXAxisCheck: function(stationaryElement, event) {
+        var offset = stationaryElement.offset()
+        var leftOfElement = offset.left
+        var rightOfElement = offset.left + stationaryElement.width()
+
+        return (event.pageX < rightOfElement && event.pageX > leftOfElement)
+      },
+
+      scrollListUp: function(stationaryElement, amount) {
+        stationaryElement.scrollTop(stationaryElement.scrollTop() - amount)
+      },
+
+      scrollListDown: function(stationaryElement, amount) {
+        stationaryElement.scrollTop(stationaryElement.scrollTop() + amount)
+      }
+    },
+    
+    browserSubmit: function (action, method, input) {
+      var form = $('<form />', {
         action: action,
         method: method,
         style: 'display: none;'
-    });
-    if (typeof input !== 'undefined') {
+      });
+      if (typeof input !== 'undefined') {
         $.each(input, function (name, value) {
-            $('<input />', {
-                type: 'hidden',
-                name: name,
-                value: value
-            }).appendTo(form);
+          $('<input />', {
+            type: 'hidden',
+            name: name,
+            value: value
+          }).appendTo(form);
         });
+      }
+      form.appendTo('body').submit();
     }
-    form.appendTo('body').submit();
   }
-}
+
+  Heorot.prototype.each = function (callback) {
+    return heorot.each(this, callback);
+  };
+
+  Heorot.f = function (callback) {
+    return 5;
+  };
+
+  Heorot.prototype.map = function (callback) {
+    return heorot.map(this, callback);
+  };
+
+  Heorot.prototype.select = function (callback) {
+    return heorot.select(this, callback);
+  };
+
+  Heorot.prototype.reject = function (callback) {
+    return heorot.reject(this, callback);
+  };
+
+  return heorot;
+}());
+H = heorot;
