fb0b5448cf5ed08cbd4ecdc0e1542d7eb0ae94c8
Eric Stensland <estensland88@gmail.com>
2014-09-11 08:06:26 -0500

    Update heorot.js

--- Heorot-JS/heorot.js
+++ Heorot-JS/heorot.js
@@ -92,38 +92,6 @@ window.heorot = (function(){
       $(parent).append(sections);
     },
     
-    scrollHelper : {
-      scrollCheck: function(stationaryElement, event, amount){
-        if (this.insideXAxisCheck(stationaryElement, event)) {
-          var topOfElement = stationaryElement.offset().top;
-
-          if (topOfElement > event.pageY){
-            this.scrollListUp(stationaryElement, amount);
-          }
-
-          else if (topOfElement + stationaryElement.height() < event.pageY) {
-            this.scrollListDown(stationaryElement, amount);
-          }
-        }
-      },
-
-      insideXAxisCheck: function(stationaryElement, event) {
-        var offset = stationaryElement.offset();
-        var leftOfElement = offset.left;
-        var rightOfElement = offset.left + stationaryElement.width();
-
-        return (event.pageX < rightOfElement && event.pageX > leftOfElement);
-      },
-
-      scrollListUp: function(stationaryElement, amount) {
-        stationaryElement.scrollTop(stationaryElement.scrollTop() - amount);
-      },
-
-      scrollListDown: function(stationaryElement, amount) {
-        stationaryElement.scrollTop(stationaryElement.scrollTop() + amount);
-      }
-    },
-    
     browserSubmit: function (action, method, input) {
       var form = $('<form />', {
         action: action,
