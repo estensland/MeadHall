af54cb96ce8298b3ec360fda424cfbbd8542f724
Eric Stensland <estensland88@gmail.com>
2014-09-17 21:40:06 -0500

    splitting up random ruby files

--- Ruby_Formulas/random_ruby_files.rb
+++ Ruby_Formulas/random_ruby_files.rb
@@ -378,38 +378,6 @@ end
 puts format_ssns("444.44.5555") == "444-44-5555" 
 puts format_ssns("444.44.5555, AAA-AA-AAAA") == ("444-44-5555, AAA-AA-AAAA")
 
-
-
-# MODE OF AN ARRAY
-
-def mode(array)
-  counter = Hash.new(0) # Create an empty hash to place numbers from array in
- 
-  array.each do |i| # Iterate over each element of the array and place in the hash.
-    counter[i] += 1 # Every time a key comes up, add 1 to the value to count the occurences 
-  end
- 
-  mode_array = [] # Create an array that will eventually have the mode pushed in it
- 
-  counter.each do |k, v| # iterate over each value of the hash
-    if v == counter.values.max # Check if the value is equal to the max key value
-      mode_array << k # Push the key of the values that match the highest value into mode_array.  
-    end
-  end
- 
-  mode_array #Return the new array
-end
-
-
-# MEDIAN OF AN ARRAY
-
-def median(array)
-  sorted = array.sort
-  x = sorted.length
-  return (sorted[(x - 1) / 2] + sorted[x / 2]) / 2.0
-end
-
-
 # AVERAGE OF AN ARRAY PLUS CASE METHOD, #GRADE
 
 def get_grade(array)
@@ -428,12 +396,4 @@ def get_grade(array)
     else
     "ERROR"
   end
-end
-
-# SUM OF AN ARRAY
-
-def total(array)
-  sum = 0
-  array.each {|i| sum += i}
-  return sum
 end
