ad0cc034b00dbe567f8f0c3f49188a91c57effeb
Eric Stensland <estensland88@gmail.com>
2014-09-22 20:55:05 -0500

    fixed stuff

--- Ruby_Formulas/chance_distribution.rb
+++ Ruby_Formulas/chance_distribution.rb
@@ -1,22 +1,40 @@
-module ChanceDistribution
+class WeightedDistribution
+  def initialize(opts)
+    @opts = opts
+  end
 
-
-  def get_grade(array)
-    average = array.inject(:+)/array.length
-    case average
-      when 90..100
-        "A"
-      when 80..90 
-        "B"
-      when 70..80
-        "C"
-      when 60..70
-        "D"
-      when 0..70
-        "F"
-      else
-      "ERROR"
+  def value_of(input)
+    @opts.each do |key, value|
+      if value.is_a?(Range)
+        test_value = value.first.is_a?(Integer) ? input.to_i : input.to_s
+        return key if value.include?(test_value)
+      elsif value.is_a?(Integer)
+        return key if value == input.to_i
+      elsif value.is_a?(String)
+        test_value =  value =~ /^\d+$/ ? value.to_i : input
+        return key if value == test_value
+      end
     end
   end
 
+  def random_value
+    rand_value = rand(@opts.values.first.min..@opts.values.last.max)
+    self.value_of(rand_value)
+  end
 end
+
+
+percent_per_continent = WeightedDistribution.new({
+                                                  'Africa' => 1..23,
+                                                  'Asia' => '24'..'80',
+                                                  'Europe' => 81..85,
+                                                  'Americas' => 86..99,
+                                                  'Oceania' => 100
+                                                })
+
+p percent_per_continent.value_of(5) == 'Africa'
+p percent_per_continent.value_of('5') == 'Africa'
+p percent_per_continent.value_of(24) == 'Asia'
+p percent_per_continent.value_of('77') == 'Asia'
+p percent_per_continent.value_of(100) == 'Oceania'
+p percent_per_continent.value_of('100') == 'Oceania'
