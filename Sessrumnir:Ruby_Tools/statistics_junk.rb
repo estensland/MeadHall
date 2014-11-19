2ad2064bdd33e02a52fbf4c039cab7a19c0cb144
Eric <estensland88@gmail.com>
2014-11-19 07:43:52 -0600

    gave variables more readable names

--- Sessrumnir:Ruby_Tools/statistics_junk.rb
+++ Sessrumnir:Ruby_Tools/statistics_junk.rb
@@ -15,15 +15,15 @@ module StatisticsJunk
   def mode
     counter = Hash.new(0) # Create an empty hash to place numbers from array in
 
-    self.each do |i| # Iterate over each element of the array and place in the hash.
-      counter[i] += 1 # Every time a key comes up, add 1 to the value to count the occurences
+    self.each do |element| # Iterate over each element of the array and place in the hash.
+      counter[element] += 1 # Every time a key comes up, add 1 to the value to count the occurences
     end
 
     mode_array = [] # Create an array that will eventually have the mode pushed in it
 
-    counter.each do |k, v| # iterate over each value of the hash
-      if v == counter.values.max # Check if the value is equal to the max key value
-        mode_array << k # Push the key of the values that match the highest value into mode_array.
+    counter.each do |key, value| # iterate over each value of the hash
+      if value == counter.values.max # Check if the value is equal to the max key value
+        mode_array << key # Push the key of the values that match the highest value into mode_array.
       end
     end
 
@@ -33,15 +33,14 @@ module StatisticsJunk
   # MEDIAN
   def median
     sorted = self.sort
-    x = sorted.length
-    (sorted[(x - 1) / 2] + sorted[x / 2]) / 2.0
+    leng = sorted.length
+    (sorted[(leng - 1) / 2] + sorted[leng / 2]) / 2.0
   end
 
   # STANDARD DEVIATION
   def standard_deviation
-    u = self.mean #cache the mean
-    result = self.inject(0){|sum, num| sum + ((num - u) ** 2)} # sum of the squared variance for each item
+    av = self.mean #cache the mean
+    result = self.inject(0){|sum, num| sum + ((num - av) ** 2)} # sum of the squared variance for each item
     Math.sqrt((result.to_f / self.length)) # Get the the square root of the mean of the sumed squared variance
   end
-
 end
