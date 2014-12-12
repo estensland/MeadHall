2615238b390b016f09d1d3988457d756b068c0ca
Eric Stensland <estensland88@gmail.com>
2014-12-12 08:01:45 -0600

    setting up board

--- Ruby_Battleship/board.rb
+++ Ruby_Battleship/board.rb
@@ -1,5 +1,25 @@
 require_relative 'requirement'
 
 class Board
+  def initialize(opts)
+    @grid =[[0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0]]
+    if opts[:manual_setup]
+      player_setup
+    else
+      auto_set_up
+    end
+  end
 
+  def auto_set_up
+    [[[Carrier], ['A4', 'B4', 'C4', 'D4', 'E4'], [Battleship], ['A7', 'B7', 'C7', 'D7']]]
+  end
 end
