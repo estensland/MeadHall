c04e2a607bb45b33d687b79eba0663caa8880800
Eric Stensland <estensland88@gmail.com>
2014-12-13 11:01:35 -0600

    working on sinking dat battleship

--- Ruby_Battleship/game_controller.rb
+++ Ruby_Battleship/game_controller.rb
@@ -1,27 +1,45 @@
 require_relative 'requirement'
 
-class Board
+class GameController
   DEFAULT_SETUP = [[[Carrier], ['A4', 'B4', 'C4', 'D4', 'E4']], [[Battleship], ['A7', 'B7', 'C7', 'D7']], [[Cruiser], ['A9', 'B9', 'C9']], [[Submarine], ['A10', 'B10', 'C10']], [[Destroyer], ['A1', 'B1']]]
 
   def initialize(opts)
-    @grid =[[0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0]]
-    if opts[:manual_setup]
+    players_setup
+  end
+
+  def players_setup
+    res = [1,2].map do |i|
+      @player1 = player_setup(i)
+    end
+  end
+
+  def player_setup_questions
+  end
+
+  def player_setup(num)
+    puts "Player #{num} Setup"
+    puts ""
+
+    name = player_name_setup
+  end
+
+  def player_name_setup
+    puts 'Rock Your Name:'
+    name = gets.chomp
+  end
+
+    puts '1. Manual setup
+          2. Auto Setup'
+    name = gets.chomp
+
+    if input
       player_setup
     else
       auto_set_up
     end
   end
 
-  def auto_set_up
+
     DEFAULT_SETUP.each do |ship, coordinates|
       ship_place(ship, coordinates)
     end
@@ -29,7 +47,7 @@ class Board
 
   def player_set_up
     Ship::SHIP_CLASSES.each do |ship|
-      puts 
+      puts
     end
   end
 end
--- Ruby_Battleship/player.rb
+++ Ruby_Battleship/player.rb
@@ -4,12 +4,7 @@ class Player
 
   def initialize(opts = {})
     @name = opts[:name]
-    @fleet = gen_fleet
-  end
-
-  def gen_fleet
-    Ship::SHIP_CLASSES
-    @fleet
+    @fleet = opts[:fleet]
   end
 
 end
