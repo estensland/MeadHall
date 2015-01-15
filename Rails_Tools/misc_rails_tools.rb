fbe03b16c89837e9c86fb35be71cc9ae8837edd4
Eric Stensland <estensland88@gmail.com>
2015-01-14 21:03:16 -0600

    Update and rename table_size.rb to misc_rails_tools.rb

---
+++ Rails_Tools/misc_rails_tools.rb
@@ -0,0 +1,16 @@
+module MiscRailsTools
+  def pg_table_sizes
+    query = <<EOF
+    SELECT
+      table_schema || '.' || table_name AS table_full_name,
+      pg_size_pretty(pg_total_relation_size('"' || table_schema || '"."' || table_name || '"')) AS size
+    FROM information_schema.tables
+    ORDER BY
+      pg_total_relation_size('"' || table_schema || '"."' || table_name || '"') DESC
+EOF
+    
+    result = ActiveRecord::Base.connection.execute(query)
+    
+    result.to_a 
+  end
+end
