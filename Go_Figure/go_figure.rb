f7da57535956ccb18882d5cf115c96eabd91bd34
Eric Stensland <estensland88@gmail.com>
2015-01-31 15:48:03 -0600

    updated go figure puzzle solver to ignore the duplicate thingy

--- Go_Figure/go_figure.rb
+++ Go_Figure/go_figure.rb
@@ -1,18 +1,19 @@
-ops = [   :-, :*,
-        :*, :*,  :*,
+ops = [   :+, :*,
+        :/, :*,  :+,
           :*, :-,
-        :-, :+,  :+,
-          :+, :*
+        :+, :/,  :+,
+          :/, :+
       ]
 
-results = [         30,
-                    29,
-                    12,
-          27,  21,  20]
+results = [         13,
+                    16,
+                    11,
+          11,  12,  10]
 
-numbers = [1,2,2,3,4,5,6,7,8]
+numbers = [1,2,2,3,4,6,7,8,9]
 
 def go_figure(nums, operators, answers)
+  res = []
   nums.permutation.each do |i|
     if i[0].send(operators[0], i[1]).send(operators[1], i[2]) == answers[0]
       if i[3].send(operators[5], i[4]).send(operators[6], i[5]) == answers[1]
@@ -20,16 +21,20 @@ def go_figure(nums, operators, answers)
           if i[0].send(operators[2], i[3]).send(operators[7], i[6]) == answers[3]
             if i[1].send(operators[3], i[4]).send(operators[8], i[7]) == answers[4]
               if i[2].send(operators[4], i[5]).send(operators[9], i[8]) == answers[5]
-                p i[0..2]
-                p i[3..5]
-                p i[6..8]
-                p ' '
+                res << i
               end
             end
           end
         end
       end
     end
+  end
+  
+  res.uniq.each do |result|
+    p result[0..2]
+    p result[3..5]
+    p result[6..8]
+    p ' '
   end
 end
 
