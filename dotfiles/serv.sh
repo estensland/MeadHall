1b803cb977b86c4a3a6aa71a4c7ba4ffff622e3b
Eric Stensland <estensland88@gmail.com>
2015-02-28 11:27:21 -0600

    updated aliases

--- dotfiles/serv.sh
+++ dotfiles/serv.sh
@@ -9,16 +9,27 @@ alias goapp='cd /home/rails'
 
 alias reuni='service unicorn restart'
 
+rc(){
+  goapp
+  rails c production
+}
+
 rerock() {
   echo
   echo "pulling code from master then restarting unicorn"
+  goapp
   echo
   echo "pulling from remote origin master"
   hitme
+  echo
+  echo "precompiling assets"
+  echo
+  rake assets:precompile RAILS_ENV=production
+  echo
   echo
   echo "restarting unicorn"
   echo
   reuni
 }
 
-# PS1='${debian_chroot:+($debian_chroot)}\[\033[38;1m\033[33;34m\u\033[00m\]\033[33;31m@\033[00m\]\033[01;34$
+# PS1='${debian_chroot:+($debian_chroot)}\[\033[38;1m\033[33;34m\u\033[33;31m@\033[01;34m\]\H\033[31m:\033[36m\t\033[31m:\033[01;34m\w\033[01;31;1m\] ⌘  \033[00m\]'
