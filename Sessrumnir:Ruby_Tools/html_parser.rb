e0961405091dd3bce2b5c9ae190aeba8286e088a
Eric Stensland <estensland88@gmail.com>
2015-04-04 22:15:45 -0500

    Update html_parser.rb

--- Sessrumnir:Ruby_Tools/html_parser.rb
+++ Sessrumnir:Ruby_Tools/html_parser.rb
@@ -4,11 +4,11 @@ def table_parse(table_data, attrs)
   table_data.gsub(/(<tr>|<td>)/, '').split('</tr>').map do |row|
     traits = row.split('</td>').map(&:strip)
     result = {}
-    
+
     attrs.each.with_index do |attrib, index|
       result[attrib] = traits[index]
     end
-    
+
     result
   end
 end
