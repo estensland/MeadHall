d30b0b20c0c7d69da50962c6ee82bde126c3b12d
Eric Stensland <estensland88@gmail.com>
2015-06-09 13:41:15 -0500

    Update .zeus_aliases.sh

--- dotfiles/aliases/.zeus_aliases.sh
+++ dotfiles/aliases/.zeus_aliases.sh
@@ -6,21 +6,27 @@ alias zs='zeus start'
 
 function ztc(){
   if [ $# -gt 1 ]; then
-      echo testing line $2 in $1 controller
-      zeus rspec spec/controllers/$1_controller_spec.rb:$2;
+    echo testing line $2 in $1 controller
+    zeus rspec spec/controllers/$1_controller_spec.rb:$2;
+  elif [ $# -gt 0 ]; then
+    echo testing all of $1 controller
+    zeus rspec spec/controllers/$1_controller_spec.rb;
   else
-      echo testing all of $1 controller
-      zeus rspec spec/controllers/$1_controller_spec.rb;
+    echo testing all controllers
+    zeus rspec spec/controllers/;
   fi
 }
 
 function ztm(){
   if [ $# -gt 1 ]; then
-      echo testing line $2 in $1 model
-      zeus rspec spec/models/$1_spec.rb:$2;
+    echo testing line $2 in $1 model
+    zeus rspec spec/models/$1_spec.rb:$2;
+  elif [ $# -gt 0 ]; then
+    echo testing all of $1 model
+    zeus rspec spec/models/$1_spec.rb;
   else
-      echo testing all of $1 model
-      zeus rspec spec/models/$1_spec.rb;
+    echo testing all of models
+    zeus rspec spec/models/;
   fi
 }
 
