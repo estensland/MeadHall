2929dd3d90b38ced3fc55b6247da14748891d2da
Eric Stensland <estensland88@gmail.com>
2015-06-09 13:44:40 -0500

    Update .rspec_aliases.sh

--- dotfiles/aliases/.rspec_aliases.sh
+++ dotfiles/aliases/.rspec_aliases.sh
@@ -2,21 +2,33 @@
 
 function rtc(){
   if [ $# -gt 1 ]; then
-   echo testing line $2 in $1 controller
-   rake SPEC='spec/controllers/$1_controller_spec.rb:$2';
+    echo testing line $2 in $1 controller
+    echo "rspec spec/controllers/$1_controller_spec.rb:$2;"
+    rspec spec/controllers/$1_controller_spec.rb:$2;
+  elif [ $# -gt 0 ]; then
+    echo testing all of $1 controller
+    echo "rspec spec/controllers/$1_controller_spec.rb"
+    rspec spec/controllers/$1_controller_spec.rb;
   else
-   echo testing all of $1 controller
-   rake SPEC='spec/controllers/$1_controller_spec.rb';
+    echo testing all controllers
+    echo "rspec spec/controllers/"
+    rspec spec/controllers/;
   fi
 }
 
 function rtm(){
   if [ $# -gt 1 ]; then
-   echo testing line $2 in $1 model
-   rake SPEC='spec/models/$1_spec.rb:$2';
+    echo testing line $2 in $1 model
+    echo "rspec spec/models/$1_spec.rb:$2;"
+    rspec spec/models/$1_spec.rb:$2;
+  elif [ $# -gt 0 ]; then
+    echo testing all of $1 model
+    echo "rspec spec/models/$1_spec.rb;"
+    rspec spec/models/$1_spec.rb;
   else
-   echo testing all of $1 model
-   rake SPEC='spec/models/$1_spec.rb';
+    echo testing all of models
+    echo "rspec spec/models/;"
+    rspec spec/models/;
   fi
 }
 
@@ -25,7 +37,5 @@ rspec-help () {
   echo
   echo "rtm() = rake SPEC='spec/models/$1_spec.rb:$2'"
   echo "rtc() = rake SPEC='spec/controllers/$1_controller_spec.rb:$2'"
-  echo
-  echo "fuck_it = kill server, db:reset, rails s"
   echo
 }
