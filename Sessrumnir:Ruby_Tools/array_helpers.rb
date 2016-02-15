dbbeee6f8f62a0bbe3647e6ba5b64122823e3862
Eric Stensland <estensland88@gmail.com>
2016-02-15 09:11:29 -0600

    Create array_helpers.rb

---
+++ Sessrumnir:Ruby_Tools/array_helpers.rb
@@ -0,0 +1,15 @@
+module ArrayHelpers
+
+  def n_multiples(array, num = 2, operan = :>=)
+    multiples_finder(array, num, :==)
+  end
+  
+  def at_least_n_multiples(array, num = 2)
+    multiples_finder(array, num, :>=)
+  end
+  
+  def multiples_finder(array, num = 2, operan = :>=)
+    array.group_by{ |e| e }.select { |k, v| v.size.send(operan.to_sym, num) }.map(&:first)
+  end
+
+end
