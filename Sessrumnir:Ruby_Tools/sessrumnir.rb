9c84193cd40c83e4c89d3a69b6ee6e65147b59fc
Eric Stensland <estensland88@gmail.com>
2016-02-15 09:12:08 -0600

    Update sessrumnir.rb

--- Sessrumnir:Ruby_Tools/sessrumnir.rb
+++ Sessrumnir:Ruby_Tools/sessrumnir.rb
@@ -1,6 +1,7 @@
 module Sessrumnir
   require_relative 'chance_distribution'
   require_relative 'rubymathics'
+  require_relative 'array_helpers'
   require_relative 'SSN'
   require_relative 'statistics_junk'
   require_relative 'string_formatting'
